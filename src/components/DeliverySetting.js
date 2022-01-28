3c2f43c7214ea9194f9c66a822de69f0bb83b52c
dawon <lyumilove9@gmail.com>
2022-01-29 01:51:52 +0900

    [Feat] - 토글버튼 기능 구현 close #32

--- src/components/DeliverySetting.js
+++ src/components/DeliverySetting.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ToggleBtn from 'components/ToggleBtn';
 import Preorder from 'components/Preorder';
 import 'utils/styles/DeliverySetting.scss';
 
 function DeliverySetting() {
+  const [departure, setDeparture] = useState(false);
+  const [pickup, setPickup] = useState(false);
+  const [preorder, setPreorder] = useState(false);
+
+  useEffect(() => {
+    if (departure || pickup) {
+      setPreorder(false);
+    }
+  }, [departure, pickup]);
+
+  useEffect(() => {
+    if (preorder) {
+      setDeparture(false);
+      setPickup(false);
+    }
+  }, [preorder]);
+
   return (
     <div className='delivery-setting-container'>
       <div className='delivery-setting-title'>
@@ -12,16 +29,16 @@ function DeliverySetting() {
       <div className='delivery-setting'>
         <div className='delivery-title'>사용자 배송일 출발일 지정</div>
         <div className='delivery-toggle'>
-          <ToggleBtn />
+          <ToggleBtn on={departure} setOn={setDeparture} />
         </div>
         <div className='delivery-title'>방문 수령</div>
         <div className='delivery-toggle'>
-          <ToggleBtn />
+          <ToggleBtn on={pickup} setOn={setPickup} />
         </div>
         <div className='delivery-title'>선 주문 예약 배송</div>
         <div className='delivery-toggle preOrder'>
           <div>
-            <ToggleBtn />
+            <ToggleBtn on={preorder} setOn={setPreorder} />
           </div>
           <div className='preorder-container'>
             <Preorder />
