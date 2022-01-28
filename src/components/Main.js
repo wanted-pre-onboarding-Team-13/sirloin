d906d79b0be1ea3c8ab11e5b0dc420d7ad51625d
abg3000 <abg3000@naver.com>
2022-01-28 11:10:27 +0900

    [Fix] 충돌 해결

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,32 +7,24 @@ import PartThree from './PartThree/PartThree';
 
 function Main() {
   return (
-    <div className="main-layout">
-      <div className="sirloin-logo">
+    <div className='main-layout'>
+      <div className='sirloin-logo'>
         <span>
-          <img src="https://www.sirloin.co.kr/data/skin/front/sirloin_2_6_0v/img/sirloin/logo.png"></img>
+          <img src='https://www.sirloin.co.kr/data/skin/front/sirloin_2_6_0v/img/sirloin/logo.png'></img>
         </span>
       </div>
-      <div className="sirloin-menu ">
+      <div className='sirloin-menu '>
         <Menulist />
       </div>
-      <div className="sirloin-save">
-        <div className="save-container">
+      <div className='sirloin-save'>
+        <div className='save-container'>
           <span>상품 등록</span>
-          <button className="btn-save">저장하기</button>
+          <button className='btn-save'>저장하기</button>
         </div>
       </div>
-<<<<<<< HEAD
       <div className='sirloin-form'>
         <Durationconfig />
-        <div className='test'></div>
-=======
-      <div className="sirloin-form">
-        <PartThree />
-        <div className="test"></div>
-        <div className="test"></div>
-        <div className="test"></div>
->>>>>>> sirloin/part3
+        <PartThree /> <div className='test'></div>
       </div>
     </div>
   );
