31937da646667134ad402299f0f83b31b1472ac3
dawon <lyumilove9@gmail.com>
2022-01-28 16:37:44 +0900

    [Style] - 배송설정 레이아웃 수정

--- src/components/Preorder.js
+++ src/components/Preorder.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Calendar from 'components/Calendar';
+import 'utils/styles/Preorder.scss';
 
 function Preorder() {
   const [order, setOrder] = useState({ start_date: new Date(), end_date: new Date() });
@@ -16,13 +17,21 @@ function Preorder() {
 
   return (
     <div className='preorder-container'>
-      <div className='time'>주문 시간</div>
-      <Calendar mode='show' config={order} setConfig={setOrder} />
+      <div className='time title'>
+        주문 시간
+        <Calendar mode='show' config={order} setConfig={setOrder} />
+      </div>
+
       <div className='delivery-option'>
-        <div>새벽 배송</div>
-        <Calendar config={midnight} setConfig={setMidnight} />
-        <div>일반 배송</div>
-        <Calendar config={normal} setConfig={setNormal} />
+        <div className='title'>
+          새벽 배송
+          <Calendar config={midnight} setConfig={setMidnight} />
+        </div>
+
+        <div className='title'>
+          일반 배송
+          <Calendar config={normal} setConfig={setNormal} />
+        </div>
       </div>
     </div>
   );
